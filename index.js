5dfb45ab8bc38f0d25879b5b656f56f816a1559a
r-keegan <rebeccachristinekeegan@gmail.com>
2019-03-19 22:32:45 +0000

    updated index to only tell joke when user input specially says 'joke'

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-var request = require("request")
-var SlackBot = require("slackbots")
+var request = require("request");
+var SlackBot = require("slackbots");
 
 
-const envKey = process.env.JOKES_BOT_TOKEN
+const envKey = process.env.JOKES_BOT_TOKEN;
 
 //create a bot
 var bot = new SlackBot({
     token: envKey,
     name: "Jokes Bot"
-})
+});
 
 bot.on("message", msg => {
     switch (msg.type) {
         case "message":
-            if (msg.channel[0] === "D" && msg.bot_id === undefined) {
-                getRandomJoke(postMessage, msg.user)
+            let msgText = msg.text;
+            console.log(msgText);
+            if (msgText.toUpperCase().includes("JOKE")) {
+                if (msg.channel[0] === "D" && msg.bot_id === undefined) {
+                    getRandomJoke(postMessage, msg.user);
+                }
             }
-            break
+            break;
+
     }
-})
+});
 
 const postMessage = (message, user) => {
     bot.postMessage(user, message, { as_user: true })
-}
+};
 
 const getRandomJoke = (callback, user) => {
     return request("https://icanhazdadjoke.com/slack", (error, response) => {
         if (error) {
             console.log("Error: ", error)
         } else {
-            let jokeJSON = JSON.parse(response.body)
-            let joke = jokeJSON.attachments[0].text
+            let jokeJSON = JSON.parse(response.body);
+            let joke = jokeJSON.attachments[0].text;
             return callback(joke, user)
         }
     })
-}
+};
