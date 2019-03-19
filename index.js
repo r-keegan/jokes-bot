fd358a95eecc460e585d33d46d212ee959fbbadc
r-keegan <rebeccachristinekeegan@gmail.com>
2019-03-19 15:44:44 +0000

    tidied up code

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var request = require("request");
-var SlackBot = require('slackbots');
+var request = require("request")
+var SlackBot = require("slackbots")
 
-// create a bot
-// var bot = new SlackBot({
-//     token: 'xoxb-012345678-ABC1DFG2HIJ3', // Add a bot https://my.slack.com/services/new/bot and put the token
-//     name: 'My Bot'
-// });
 
 const envKey = process.env.JOKES_BOT_TOKEN
+
+//create a bot
 var bot = new SlackBot({
     token: envKey,
     name: "Jokes Bot"
@@ -23,6 +20,10 @@ bot.on("message", msg => {
     }
 })
 
+const postMessage = (message, user) => {
+    bot.postMessage(user, message, { as_user: true })
+}
+
 const getRandomJoke = (callback, user) => {
     return request("https://icanhazdadjoke.com/slack", (error, response) => {
         if (error) {
@@ -34,27 +35,3 @@ const getRandomJoke = (callback, user) => {
         }
     })
 }
-
-const postMessage = (message, user) => {
-    bot.postMessage(user, message, { as_user: true })
-}
-
-// bot.on('start', function() {
-//     // more information about additional params https://api.slack.com/methods/chat.postMessage
-//     var params = {
-//         icon_emoji: ':cat:'
-//     };
-//
-//     // define channel, where bot exist. You can adjust it there https://my.slack.com/services
-//     bot.postMessageToChannel('random', 'meow!', params);
-//
-//     // define existing username instead of 'user_name'
-//     bot.postMessageToUser('user_name', 'meow!', params);
-//
-//     // If you add a 'slackbot' property,
-//     // you will post to another user's slackbot channel instead of a direct message
-//     bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' });
-//
-//     // define private group instead of 'private_group', where bot exist
-//     bot.postMessageToGroup('private_group', 'meow!', params);
-// });
